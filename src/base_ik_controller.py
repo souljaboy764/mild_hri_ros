d018268972d79c137449d5d33991f14c480f879e
souljaboy764 <6441764+souljaboy764@users.noreply.github.com>
2023-04-13 12:43:32 +0200

    added iterative conditioning for HSMM

--- src/base_ik_controller.py
+++ src/base_ik_controller.py
@@ -2,7 +2,8 @@ import numpy as np
 import os
 from ikpy.chain import Chain
 
-from utils.visualization import *
+# from utils import *
+from utils import *
 from nuitrack_node import NuitrackROS
 
 import rospy
@@ -15,97 +16,94 @@ from pepper_controller_server.srv import JointTarget
 from sensor_msgs.msg import JointState
 import rospkg
 rospack = rospkg.RosPack()
-default_arm_joints = [1.5708, -0.109, 0.7854, 0.009, 1.5708, 0.75] # default standing angle values
+default_arm_joints = [1.5708, -0.109, 0.7854, 0.009, 1.8239, 0.75] # default standing angle values
 
 class BaseIKController:
 	def __init__(self):
 		rospy.wait_for_service('/pepper_dcm/RightArm_controller/goal')
-		self.__send_target = rospy.ServiceProxy('/pepper_dcm/RightArm_controller/goal', JointTarget)
-		self.__state_pub = rospy.Publisher("display_robot_state", DisplayRobotState, queue_size=5)
-		self.__broadcaster = tf2_ros.StaticTransformBroadcaster()
-		self.__tfBuffer = tf2_ros.Buffer()
-		self.__listener = tf2_ros.TransformListener(self.__tfBuffer)
+		self.send_target = rospy.ServiceProxy('/pepper_dcm/RightArm_controller/goal', JointTarget)
+		self.state_pub = rospy.Publisher("display_robot_state", DisplayRobotState, queue_size=5)
+		self.broadcaster = tf2_ros.StaticTransformBroadcaster()
+		self.tfBuffer = tf2_ros.Buffer()
+		self.listener = tf2_ros.TransformListener(self.tfBuffer)
 
-		self.__target_tf = TransformStamped()
-		self.__target_tf.header.frame_id = 'base_footprint'
-		self.__target_tf.child_frame_id = 'target'
-		self.__target_tf.transform.rotation.w = 1
+		self.target_tf = TransformStamped()
+		self.target_tf.header.frame_id = 'base_footprint'
+		self.target_tf.child_frame_id = 'target'
+		self.target_tf.transform.rotation.w = 1.
 
-		self.__hand_tf = TransformStamped()
-		self.__hand_tf.header.frame_id = 'base_footprint'
-		self.__hand_tf.child_frame_id = 'hand'
-		self.__hand_tf.transform.rotation.w = 1
+		self.hand_tf = TransformStamped()
+		self.hand_tf.header.frame_id = 'base_footprint'
+		self.hand_tf.child_frame_id = 'hand'
+		self.hand_tf.transform.rotation.w = 1.
 
 		# To visualise the expected state. Using both left and right arms otherwise the left arm is always raised
-		self.__state_msg = DisplayRobotState()
-		self.__state_msg.state.joint_state.header.frame_id = "base_footprint"
+		self.state_msg = DisplayRobotState()
+		self.state_msg.state.joint_state.header.frame_id = "base_footprint"
 
-		self.__joint_names = ["RShoulderPitch", "RShoulderRoll", "RElbowYaw", "RElbowRoll", "RWristYaw", 'RHand']
+		self.joint_names = ["RShoulderPitch", "RShoulderRoll", "RElbowYaw", "RElbowRoll", "RWristYaw", 'RHand']
 		self.joint_trajectory = JointTrajectory()
 		self.joint_trajectory.header.frame_id = "base_footprint"
-		self.joint_trajectory.joint_names = self.__joint_names
+		self.joint_trajectory.joint_names = self.joint_names
 		self.joint_trajectory.points.append(JointTrajectoryPoint())
-		self.joint_trajectory.points[0].effort = np.ones(len(self.__joint_names)).tolist()
+		self.joint_trajectory.points[0].effort = np.ones(len(self.joint_names)).tolist()
 		self.joint_trajectory.points[0].effort[0] = 0.5
 		self.joint_trajectory.points[0].positions = default_arm_joints
 		self.joint_trajectory.header.stamp = rospy.Time.now()
 
-		self.__pepper_chain = Chain.from_json_file(os.path.join(rospack.get_path('segmint-ik'), "resources", "pepper", "pepper_right_arm.json"))
+		self.pepper_chain = Chain.from_json_file(os.path.join(rospack.get_path('segmint-ik'), "resources", "pepper", "pepper_right_arm.json"))
 
-		self.__offset = np.array([0.0,0.0,0])
-		self.__nuitrack = NuitrackROS(height=480, width=848, horizontal=False)
-		self.__ik_result = [0.0, 1.5708, -0.109, 0.7854, 0.009, 0.7854, 0.0 , 0.0, 0.0]
+		self.offset = np.array([0.0,0.0,0])
+		self.nuitrack = NuitrackROS(height=480, width=848, horizontal=False)
+		self.ik_result = [0.0, 1.5708, -0.109, 0.7854, 0.009, 1.8239, 0.0 , 0.0, 0.0]
 
-		self.__joint_state_sub = rospy.Subscriber('/joint_states', JointState, self.joint_state_cb)
+		self.joint_state_sub = rospy.Subscriber('/joint_states', JointState, self.joint_state_cb)
 
-	def joint_state_cb(self, msg):
+	def joint_state_cb(self, msg:JointState):
 		if len(msg.name)<=6:
 			return
-		self.__state_msg.state.joint_state = msg
-		self.__state_msg.state.joint_state.position = list(self.__state_msg.state.joint_state.position)
-		self.__state_msg.state.joint_state.position[11:17] = list(self.joint_trajectory.points[0].positions)
+		self.joint_readings = msg.position[11:17]
+		print(self.joint_readings[4])
+		self.state_msg.state.joint_state = msg
+		self.state_msg.state.joint_state.position = list(self.state_msg.state.joint_state.position)
+		self.state_msg.state.joint_state.position[11:17] = list(self.joint_trajectory.points[0].positions)
 
 	def observe_human(self):
-		img, nui_skeleton, stamp = self.__nuitrack.update()
+		img, nui_skeleton, stamp = self.nuitrack.update()
 		if img is None or len(nui_skeleton)==0:
 			return [], None, stamp
 		
-		hand_pose = self.__nuitrack.base2cam[:3,:3].dot(nui_skeleton[-1, :]) + self.__nuitrack.base2cam[:3,3]
-		self.__hand_tf.transform.translation.x = hand_pose[0]
-		self.__hand_tf.transform.translation.y = hand_pose[1]
-		self.__hand_tf.transform.translation.z = hand_pose[2]
-		
-		target_pose = hand_pose - self.__offset
-		self.__target_tf.transform.translation.x = target_pose[0]
-		self.__target_tf.transform.translation.y = target_pose[1]
-		self.__target_tf.transform.translation.z = target_pose[2]
+		hand_pose = self.nuitrack.base2cam[:3,:3].dot(nui_skeleton[-1, :]) + self.nuitrack.base2cam[:3,3]
+		target_pose = hand_pose - self.offset
 
+		self.hand_tf.transform = mat2TF(hand_pose)
+		self.target_tf.transform = mat2TF(target_pose)
+		
 		return nui_skeleton, hand_pose, stamp
 	
 	def publish(self, stamp):
-		self.__state_msg.state.joint_state.header.stamp = self.__target_tf.header.stamp = self.__hand_tf.header.stamp = self.joint_trajectory.header.stamp = stamp
-		self.__send_target(self.joint_trajectory)
-		self.__broadcaster.sendTransform([self.__target_tf, self.__hand_tf])
-		self.__state_pub.publish(self.__state_msg)
+		self.state_msg.state.joint_state.header.stamp = self.target_tf.header.stamp = self.hand_tf.header.stamp = self.joint_trajectory.header.stamp = stamp
+		self.send_target(self.joint_trajectory)
+		self.broadcaster.sendTransform([self.target_tf, self.hand_tf])
+		self.state_pub.publish(self.state_msg)
 
-	def hand_in_baselink(self, hand_pose):
+	def in_baselink(self, hand_pose):
 		# Need pose in base_link frame for IK
-		link_TF = self.__tfBuffer.lookup_transform('base_link', 'base_footprint', rospy.Time())
-		link_transform = quaternion_matrix([link_TF.transform.rotation.x, link_TF.transform.rotation.y, link_TF.transform.rotation.z, link_TF.transform.rotation.w])
-		link_transform[:3,3] = [link_TF.transform.translation.x, link_TF.transform.translation.y, link_TF.transform.translation.z]
+		link_TF = self.tfBuffer.lookup_transform('base_link', 'base_footprint', rospy.Time())
+		link_transform = ROS2mat(link_TF.transform)
 		hand_pose = link_transform[:3,:3].dot(hand_pose) + link_transform[:3,3]
-		return hand_pose - self.__offset
+		return hand_pose - self.offset
 	
 	def step(self, nui_skeleton, hand_pose):
 		if len(nui_skeleton)==0 or hand_pose is None:
 			return
-		target_pose = self.hand_in_baselink(hand_pose)
+		target_pose = self.in_baselink(hand_pose)
 		frame_target = np.eye(4)
 		frame_target[:3, 3] = target_pose
-		self.__ik_result = self.__pepper_chain.inverse_kinematics_frame(frame_target, initial_position=self.__ik_result)
+		self.ik_result = self.pepper_chain.inverse_kinematics_frame(frame_target, initial_position=self.ik_result)
 
-		rarm_joints = self.__ik_result[2:6].tolist()
-		self.joint_trajectory.points[0].positions = 0.3*np.array(self.joint_trajectory.points[0].positions) + 0.7*np.array(rarm_joints + [0.0, 0.75])
+		rarm_joints = self.ik_result[2:6].tolist()
+		self.joint_trajectory.points[0].positions = 0.3*np.array(self.joint_trajectory.points[0].positions) + 0.7*np.array(rarm_joints + [1.8239, 0.75])
 	
 if __name__=='__main__':
 	rospy.init_node('hri_ik_node')
--- src/hsmmik_controller.py
+++ src/hsmmik_controller.py
@@ -15,165 +15,129 @@ from nuitrack_node import NuitrackROS
 from base_ik_controller import *
 
 class HSMMIKController(BaseIKController):
-	def __init__(self):
+	def __init__(self, nbD=10):
 		super().__init__()
-		self.__model = np.load('models/handshake_hri.npy', allow_pickle=True).item()
+		self.model = np.load('models/handshake_hri.npy', allow_pickle=True).item()
+		self.model.Pd = np.zeros((self.model.nb_states, nbD))
+		# Precomputation of duration probabilities
+		for i in range(self.model.nb_states):
+			self.model.Pd[i, :] = pbd.multi_variate_normal(np.arange(nbD), self.model.Mu_Pd[i], self.model.Sigma_Pd[i], log=False)
+			self.model.Pd[i, :] = self.model.Pd[i, :] / (np.sum(self.model.Pd[i, :])+pbd.realmin)
 		self.alpha_hsmm = None
 		self.crossed = False
 		self.started = False
 		self.history = []
 		self.predicted_segment = []
+		self.nbD = nbD
 
-		self.rotation_normalization_matrix = None
-		
-		self.fig = plt.figure(figsize=(5,5))
-		self.ax = self.fig.add_subplot(projection='3d')
-		plt.ion()
+		self.shoulder_tf = TransformStamped()
+		self.shoulder_tf.header.frame_id = 'camera_color_optical_frame'
+		self.shoulder_tf.child_frame_id = 'body_frame'
+
+		self.markerarray_msg = rviz_gmm3d(self.model, frame_id=self.shoulder_tf.child_frame_id)
+
+		self.viz_pub = rospy.Publisher('visualization_marker_array', MarkerArray, queue_size=10)
+	
+	def skeleton_transformation(self, nui_skeleton):
+		# Origin at right shoulder, like in the training data
+		rotation_normalization_matrix = np.eye(4)
+		rotation_normalization_matrix[:3, 3] = nui_skeleton[joints_idx["right_shoulder"]-1:joints_idx["right_shoulder"], :]
+		nui_skeleton -= nui_skeleton[joints_idx["right_shoulder"]-1:joints_idx["right_shoulder"], :]
+		rotation_normalization_matrix[:3, :3] = rotation_normalization(nui_skeleton)
+		nui_skeleton = rotation_normalization_matrix[:3,:3].dot(nui_skeleton.T).T
+		nui_skeleton[:, 1] *= -1
+		nui_skeleton[:, 0] *= -1
+
+		return nui_skeleton, rotation_normalization_matrix
+
+	def hsmm_init(self):
+		p_obs, _ = self.model.obs_likelihood(self.history, None, slice(0, 6), 1)
+		self.model._B = p_obs
+		bmx, ALPHA, S, h = fwd_init(self.model, self.nbD, p_obs[:, 0])
+		self.hsmm_forward = [bmx, ALPHA, S]
+		return h/np.sum(h)
+
+	def hsmm_step(self):
+		bmx, ALPHA, S = self.hsmm_forward
+		p_obs, _ = self.model.obs_likelihood(self.history[-1:], None, slice(0, 6), 1)
+		self.model._B = np.vstack([self.model._B, p_obs])
+		bmx, ALPHA, S, h = fwd_step(self.model, bmx, ALPHA, S, self.nbD, p_obs[:, 0])
+		self.hsmm_forward = [bmx, ALPHA, S]
+		return h/np.sum(h)
 
 	def step(self, nui_skeleton, hand_pose):
 		if len(nui_skeleton)==0 or hand_pose is None:
 			return
-		# Origin at right shoulder, like in the training data
-		# if self.rotation_normalization_matrix is None:
-		self.rotation_normalization_matrix = np.eye(4)
-		self.rotation_normalization_matrix[:3, 3] = nui_skeleton[joints_idx["right_shoulder"]-1:joints_idx["right_shoulder"], :]
-		nui_skeleton -= nui_skeleton[joints_idx["right_shoulder"]-1:joints_idx["right_shoulder"], :]
-		self.rotation_normalization_matrix[:3, :3] = rotation_normalization(nui_skeleton)
-		nui_skeleton = self.rotation_normalization_matrix[:3,:3].dot(nui_skeleton.T).T
-		nui_skeleton[:, 1] *= -1
-		nui_skeleton[:, 0] *= -1
-		# else:
-		# 	nui_skeleton = self.rotation_normalization_matrix[:3,:3].dot(nui_skeleton.T).T
-		# 	nui_skeleton -= self.rotation_normalization_matrix[:3, 3]
-
-
+		
+		nui_skeleton, rotation_normalization_matrix = self.skeleton_transformation(nui_skeleton)
+		
 		if len(self.history) == 0:
-			self.history = [np.concatenate([nui_skeleton[-1,:],np.zeros_like(nui_skeleton[-1,:])])]
+			rotation_normalization_matrix[:3,:3] = rotation_normalization_matrix[:3,:3].T.dot(euler_matrix(0,0,np.pi)[:3,:3])
+			self.shoulder_tf.transform = mat2TF(rotation_normalization_matrix)
+			self.history = np.array([np.concatenate([nui_skeleton[-1,:],np.zeros_like(nui_skeleton[-1,:])])])
+			alpha_hsmm = self.hsmm_init()
+			active_segment = alpha_hsmm.argmax()
+			self.predicted_segment.append(active_segment)
 			return
 
-		self.history.append(np.concatenate([nui_skeleton[-1,:], nui_skeleton[-1,:] - self.history[-1][:3]]))
-		if not self.started and (self.history[-1][3:]**2).sum() < 0.001:
-			print((self.history[-1][3:]**2).sum())
+		if not self.started and ((nui_skeleton[-1,:] - self.history[-1, :3])**2).sum() < 0.001:
+			print('Not yet started. Current displacement:', ((nui_skeleton[-1,:] - self.history[-1, :3])**2).sum())
+			return
 		else:
 			self.started = True
-
-		alpha_hsmm = forward_variable(self.__model, len(self.history), np.array(self.history), slice(0, 6))
-		active_segment = alpha_hsmm[:,-1].argmax()
-		self.predicted_segment.append(alpha_hsmm)
-			
-		prepare_axes(self.ax)
-		if self.predicted_segment ==[]:
-			plot_pbd(self.ax, self.__model)
-		else:
-			plot_pbd(self.ax, self.__model, self.predicted_segment[-1])
-		plot_skeleton(self.ax, nui_skeleton)
+		self.history = np.vstack([self.history, np.concatenate([nui_skeleton[-1,:], nui_skeleton[-1,:] - self.history[-1, :3]])])
+		
+		alpha_hsmm = self.hsmm_step()
+		# alpha_hsmm = forward_variable(self.model, len(self.history), np.array(self.history), slice(0, 6))
+		active_segment = alpha_hsmm.argmax()
+		self.predicted_segment.append(active_segment)
+		for i in range(len(alpha_hsmm)):
+			self.markerarray_msg.markers[i].color.a = max(0.2, alpha_hsmm[i])
+		
+		mu_est_hsmm, sigma_est_hsmm = pbd.Model.condition(self.model, self.history[-1:], dim_in=slice(0, 6), dim_out=slice(6, 10), h=alpha_hsmm)
+		# self.joint_trajectory.points[0].positions[:4] = mu_est_hsmm[0]
+		self.ik_result[:4] = mu_est_hsmm[0]
+		super().step(nui_skeleton, hand_pose)
 	
 	def publish(self, stamp):
 		super().publish(stamp)
+		if self.started:
+			self.shoulder_tf.header.stamp = stamp
+			self.broadcaster.sendTransform([self.shoulder_tf])
+			self.viz_pub.publish(self.markerarray_msg)
 
 if __name__=='__main__':
 	rospy.init_node('hri_hsmmik_node')
 	controller = HSMMIKController()
 	controller.observe_human()
 	rate = rospy.Rate(100)
+	rate.sleep()
+	rate.sleep()
 	count = 0
 	while not rospy.is_shutdown():
-		if not plt.fignum_exists(controller.fig.number):
-			break
 		nui_skeleton, hand_pose, stamp = controller.observe_human()
+		if len(nui_skeleton)==0 or hand_pose is None:
+			rate.sleep()
+			continue
 		count += 1
 		if count < 20:
+			print(count)
 			controller.publish(stamp)
 			rate.sleep()
-			plt.pause(0.001)
 			continue
-		elif count >100:
-			controller.joint_trajectory.points[0].effort[0] = 0.2
-		
 		controller.step(nui_skeleton, hand_pose)
+		if controller.predicted_segment[-1] in [0, controller.model.nb_states - 1]:
+			controller.joint_trajectory.points[0].effort[0] = 0.7
+		# elif np.linalg.norm(np.array(controller.joint_readings[:4]) - np.array(controller.joint_trajectory.points[0].positions[:4])) > 0.25 :
+		elif len(controller.history) < 60:
+			controller.joint_trajectory.points[0].effort[0] = 0.7
+		else:
+			controller.joint_trajectory.points[0].effort[0] = 0.1
+		if sum(controller.predicted_segment[-6:])>=30 or (len(controller.history) > 50 and controller.predicted_segment[-1]==controller.model.nb_states - 1): # if it's the last segment, then that means you're going back to the final position
+			controller.state_msg.state.joint_state.position[11:17] = controller.joint_trajectory.points[0].positions = default_arm_joints
+			controller.publish(stamp)
+			controller.publish(stamp)
+			break
+		
 		controller.publish(stamp)
 		rate.sleep()
-		plt.pause(0.001)
-		# if hand_pose is not None:
-		# 	if prev_z is not None:
-		# 		print(hand_pose[2] - prev_z)
-		# 		if hand_pose[2] < 0.65 and hand_pose[2] - prev_z < -0.001:
-		# 			controller.joint_trajectory.points[0].positions = default_arm_joints
-		# 			controller.publish(stamp)
-		# 			rate.sleep()
-		# 			rospy.signal_shutdown('done')
-		# 	prev_z = hand_pose[2]
-
-
-		# if hand_pose is not None and hand_pose[2] < 0.8:
-		# 	stop_counter+= 1
-		# else:
-		# 	stop_counter = 0		
-		# if stop_counter>10:
-		# 	controller.joint_trajectory.points[0].positions = default_arm_joints
-		# 	controller.publish(stamp)
-		# 	rate.sleep()
-		# 	rospy.signal_shutdown('Done')
-
-	# 	cond_traj = np.array(trajectory)
-	# 	alpha_hsmm = forward_variable(model, len(cond_traj), cond_traj, slice(0, 6))
-	# 	mu_est_hsmm, sigma_est_hsmm = model.condition(cond_traj, dim_in=slice(0, 6), dim_out=slice(6, 10))
-	# 	active_segment = alpha_hsmm[:,-1].argmax()
-	# 	last_segment.append(active_segment)
-	# 	if sum(last_segment[-6:])>=30 or (len(trajectory) > 50 and active_segment==alpha_hsmm.shape[0] - 1): # if it's the last segment, then that means you're going back to the final position
-	# 		print(last_segment[-6:], sum(last_segment[-6:]))
-	# 		break
-	# 	if active_segment==0 or active_segment==alpha_hsmm.shape[0] - 1: # Handshake
-	# 	# if active_segment==0 or active_segment==1 or active_segment==alpha_hsmm.shape[0] - 1:  # Rocket
-	# 		mu_q = mu_est_hsmm[-1]
-	# 		print('NO IK')
-	# 	else:
-	# 		print('IK')
-	# 		mu_q, _ = fwd_kin.inv_kin(mu_theta=mu_est_hsmm[-1], sig_theta=np.eye(4),#sigma_est_hsmm[-1]*10,
-	# 										mu_x = target_pose, sig_x = np.eye(3)*0.01, 
-	# 										method='L-BFGS-B', jac=None, bounds=bounds,
-	# 										options={'disp': False}
-	# 								)
-		
-	# 	rarm_joints = rarm_joints*0.3 + 0.7*mu_q
-	# 	msg.state.joint_state.position[:4] = rarm_joints
-	# 	for i in range(5):
-	# 		joint_trajectory.points[i].positions = rarm_joints.tolist() + [0.0]
-	# 		# hand_trajectory.points[-1].positions = [r_hand]
-	# 	pos, _ = fwd_kin.end_effector(mu_q)
-	# 	ik_target.transform.translation.x = pos[0]
-	# 	ik_target.transform.translation.y = pos[1]
-	# 	ik_target.transform.translation.z = pos[2]
-	# 	ik_target.header.stamp = hand_tf.header.stamp = rospy.Time.now()
-		
-	# 	joint_trajectory.header.stamp = rospy.Time.now()
-	# 	robot_traj_publisher.publish(joint_trajectory)
-	# 	# hand_trajectory.header.stamp = rospy.Time.now()
-	# 	# robot_hand_publisher.publish(hand_trajectory)
-	# 	# joint_trajectory.header.stamp = rospy.Time.now()
-	# 	# robot_traj_publisher.publish(joint_trajectory)
-	# 	# joint_trajectory.header.stamp = rospy.Time.now()
-	# 	# robot_traj_publisher.publish(joint_trajectory)
-	# 	rate.sleep()
-	# 	broadcaster.sendTransform([ik_target, hand_tf])
-	# 	pub.publish(msg)
-	# 	plt.pause(0.001)
-
-
-	# print(np.array(trajectory)[:5])
-	# for i in range(5):
-	# 	joint_trajectory.points[i].positions = default_arm_joints[:4]+ [0.5]
-	# 	hand_trajectory.points[-1].positions = [0.5]
-	# joint_trajectory.header.stamp = rospy.Time.now()
-	# robot_traj_publisher.publish(joint_trajectory)
-	# hand_trajectory.header.stamp = rospy.Time.now()
-	# robot_hand_publisher.publish(hand_trajectory)
-	# rospy.Rate(1).sleep()
-	# plt.close()
-	# # joint_trajectory.header.stamp = rospy.Time.now()
-	# # robot_traj_publisher.publish(joint_trajectory)
-	# # rospy.Rate(1).sleep()
-	# # joint_trajectory.header.stamp = rospy.Time.now()
-	# # robot_traj_publisher.publish(joint_trajectory)
-	# # rospy.Rate(1).sleep()
-plt.show()
