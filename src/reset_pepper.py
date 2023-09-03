05ecbef6432f20fb1ec4afdf50c9ca724537fdb3
souljaboy764 <6441764+souljaboy764@users.noreply.github.com>
2023-09-03 11:25:36 +0200

    added node for resetting robot if needed

---
+++ src/reset_pepper.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+import numpy as np
+import rospy
+from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
+from pepper_controller_server.srv import JointTarget
+
+rospy.init_node('pepper_reset_node')
+rospy.wait_for_service('/pepper_dcm/RightArm_controller/goal')
+send_target = rospy.ServiceProxy('/pepper_dcm/RightArm_controller/goal', JointTarget)
+rate = rospy.Rate(100)
+joint_trajectory = JointTrajectory()
+joint_trajectory.header.frame_id = "base_footprint"
+joint_trajectory.joint_names = ["RShoulderPitch", "RShoulderRoll", "RElbowYaw", "RElbowRoll", "RWristYaw", 'RHand']
+joint_trajectory.points.append(JointTrajectoryPoint())
+joint_trajectory.points[0].effort = np.zeros(len(joint_trajectory.joint_names)).tolist()
+joint_trajectory.points[0].effort[0] = 0.5
+joint_trajectory.points[0].positions = [1.5708, -0.109, 0.7854, 0.009, 1.8239, 0.75] # default standing angle values
+joint_trajectory.header.stamp = rospy.Time.now()
+send_target(joint_trajectory)
+rospy.Rate(10).sleep()
+joint_trajectory.header.stamp = rospy.Time.now()
+send_target(joint_trajectory)
+rospy.Rate(10).sleep()
+joint_trajectory.header.stamp = rospy.Time.now()
+send_target(joint_trajectory)
+rospy.Rate(10).sleep()
+rospy.signal_shutdown('done')
